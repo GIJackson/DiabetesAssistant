95f9a0d5e05804328b034b799ca2d362027450f2
Guillermo Jackson <gijcancer@gmail.com>
2022-11-15 13:08:36 -0500

    Search Nutrtion Facts With Placeholder Collection View
    Created a UI that has an entry field and a search button to consume a FoodData Central REST service API using the entry text. Placeholder will be for displaying search results, hopefully.

---
+++ DiabetesAssistant/Views/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DiabetesAssistant.ViewModel;
+using System.Net.Http;
+
+namespace DiabetesAssistant;
+
+public partial class MainPage : ContentPage
+{
+	
+	public MainPage()
+	{
+		InitializeComponent();
+        BindingContext = new MainViewModel();
+	}
+
+	private string text;
+	public string Text
+	{
+		get => SearchNutritionFacts.Text;
+		set
+		{
+			if (value == null)
+				return;
+
+			Text = value;
+			OnPropertyChanged();
+
+		}
+	}
+
+	private void SearchNutrtionFactsClick_Clicked(object sender, EventArgs e)
+	{
+		
+    }
+
+}
+
